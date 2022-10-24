5d473e590bfca63936338aaeecf953db8f1cc972
cmsato09 <chrismsato@gmail.com>
2022-10-24 12:20:56 -0700

    added sqlmodel
    one-to-many table for MonsterFamily to MonsterDetail
    many-to-many table for MonsterDetail and Skill linked by
    MonsterSkillLink
    many-to-many table using MonsterBreedingLink

---
+++ model.py
@@ -0,0 +1,103 @@
+from typing import List, Optional
+from sqlmodel import Field, Relationship, SQLModel, create_engine
+
+
+class MonsterSkillLink(SQLModel, table=True):
+    monster_id: Optional[int] = Field(  # Does this need to be Optional? it will never be None
+        foreign_key='monsterdetail.id', primary_key=True
+    )
+    skill_id: Optional[int] = Field(
+        default=None, foreign_key='skill.id', primary_key=True
+    )
+
+
+class MonsterBreedingLink(SQLModel, table=True):
+    child_id: Optional[int] = Field(
+        foreign_key='monsterdetail.id', primary_key=True
+    )
+    pedigree: Optional[int] = Field(
+        foreign_key='monsterdetail.id', primary_key=True
+    )
+    parent_2: Optional[int] = Field(
+        foreign_key='monsterdetail.id', primary_key=True
+    )
+    pedigree_family: Optional[int] = Field(
+        foreign_key='monsterfamily.id', primary_key=True
+    )
+    family_2: Optional[int] = Field(
+        foreign_key='monsterfamily.id', primary_key=True
+    )
+
+
+class MonsterDetail(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    new_name: str
+    old_name: str
+    description: str
+
+    # one-to-many relation. monster in only one family category.
+    # many monsters in a family category
+    family_id: int = Field(foreign_key='monsterfamily.id')
+
+    skills: List['Skill'] = Relationship(
+        back_populates='monsters', link_model=MonsterSkillLink
+    )
+
+    monster_child: List['breeding_result'] = Relationship(
+        link_model=MonsterBreedingLink
+    )
+    monster_pedigree: List['pedigree_parent'] = Relationship(
+        link_model=MonsterBreedingLink
+    )
+    monster_parent2: List['parent_2'] = Relationship(
+        link_model=MonsterBreedingLink
+    )
+
+
+class MonsterFamily(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    family_eng: str
+    family_jpn: str
+
+    pedigree_family: List['pedigree_family'] = Relationship(
+        link_model=MonsterBreedingLink
+    )
+    family_2: List['family_2'] = Relationship(
+        link_model=MonsterBreedingLink
+    )
+
+
+class Skill(SQLModel, table=True):
+    category_type: str
+    family_type: str
+    new_name: str
+    old_name: str
+    description: str
+    mp_cost: int
+    required_level: int
+    required_hp: int
+    required_mp: int
+    required_attack: int
+    required_defense: int
+    required_speed: int
+    required_intelligence: int
+
+    upgrade: Optional[int] = Field(
+        foreign_key='skill.id', default=None,
+    )
+    # combine: Easier to make intermediate table? only a couple of skills have
+    # two or more combinations
+
+    monsters: List[MonsterDetail] = Relationship(
+        back_populates='skills', link_model=MonsterSkillLink
+    )
+
+
+class Item(SQLModel, table=True):
+    item_name: str
+    item_category: str
+    item_description: str
+    price: int
+    sell_price: int
+    sell_location: str
+
