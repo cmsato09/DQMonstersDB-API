0ce6c99497a847120a87d9e20193707cd1d2104c
cmsato09 <chrismsato@gmail.com>
2022-10-25 15:15:34 -0700

    test: created sql engine
    added code to create database and table

---
+++ app.py
@@ -0,0 +1,15 @@
+from database import create_db_and_tables, engine
+from model import Item
+
+
+def create_items():
+    pass
+
+
+def main():
+    create_db_and_tables()
+    create_items()
+
+
+if __name__== "__main__":
+    main()
