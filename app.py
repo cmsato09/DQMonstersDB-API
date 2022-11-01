8dd3a1dc95780114cb7c176a4413cfd5835d1411
cmsato09 <chrismsato@gmail.com>
2022-11-01 12:37:40 -0700

    Added skills.csv and csv.DictReader code to unpack and insert into sqlite. moved prior insertion code into pytest.py

--- app.py
+++ app.py
@@ -1,115 +1,39 @@
 from database import create_db_and_tables, engine
 from sqlmodel import Session
-
+import csv
 from model import Item, MonsterFamily, MonsterDetail, Skill, MonsterSkillLink,\
     MonsterBreedingLink, SkillCombine
 
 
-def create_items():
-    item_1 = Item(
-        item_name='Herb',item_category='Recovery',
-        item_description='Restores around 30 HP', price=10, sell_price=6,
-        sell_location='Bazaar shop 1'
-        )
-    item_2 = Item(
-        item_name='Lovewater', item_category='Recovery',
-        item_description='Restores around 70 HP', price=80, sell_price=60,
-        sell_location='Bazaar shop 1'
-        )
-    item_3 = Item(
-        item_name='Antidote', item_category='recovery',
-        item_description='Cures poison of one ally', price=10, sell_price=8,
-        sell_location='Bazaar shop 1'
-        )
-    item_4 = Item(
-        item_name='Repellent', item_category='dungeon use',
-        item_description='Reduces monster encounters', price=200,
-        sell_price=150, sell_location='Bazaar shop 1'
-        )
-    item_5 = Item(
-        item_name='BeefJerky', item_category='meat',
-        item_description="Give to monster to tame during battle or reduce "
-                         "your own monster's WLD (wildness) by 5",
-        price=200, sell_price=150, sell_location='Bazaar shop 1'
-        )
-    test_items = [item_1, item_2, item_3, item_4, item_5]
+def create_item_csv():
+    # used dictionary unpacking
+    # https://stackoverflow.com/questions/31750441/generalised-insert-into-sqlalchemy-using-dictionary
     session = Session(engine)
+    with open('csv_files/DQM1_items.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for item_entry in reader:
+            session.add(Item(**item_entry))
+    session.commit()
 
-    for item in test_items:
-        session.add(item)
+
+def create_monster_family_csv():
+    session = Session(engine)
+    with open('csv_files/DQM1_monster_family.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for family_entry in reader:
+            session.add(MonsterFamily(**family_entry))
 
     session.commit()
 
 
-def create_monster_family():
-    family_1 = MonsterFamily(family_eng='SLIME')
-    family_2 = MonsterFamily(family_eng='DRAGON')
-    family_3 = MonsterFamily(family_eng='BEAST')
-    family_4 = MonsterFamily(family_eng='BIRD')
-    family_5 = MonsterFamily(family_eng='PLANT')
-    family_6 = MonsterFamily(family_eng='BUG')
-    family_7 = MonsterFamily(family_eng='DEVIL')
-    family_8 = MonsterFamily(family_eng='UNDEAD')
-    family_9 = MonsterFamily(family_eng='MATERIAL')
-    family_10 = MonsterFamily(family_eng='???')
+def create_skill_csv():
+    session = Session(engine)
+    with open('csv_files/DQM1_skills.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for skill_entry in reader:
+            session.add(Skill(**skill_entry))
 
-    test_families = [family_1, family_2, family_3, family_4, family_5,
-                     family_6, family_7, family_8, family_9, family_10]
-    with Session(engine) as session:
-        for family in test_families:
-            session.add(family)
-
-        session.commit()
-
-    return [family_1, family_2, family_3, family_4, family_5,
-            family_6, family_7, family_8, family_9, family_10]
-
-
-def create_skill():
-    skill_1 = Skill(
-        category_type='Attack', family_type='Frizz', new_name='Frizz',
-        old_name='Blaze', description='Inflict damage with small fireball ',
-        mp_cost=2, required_level=2, required_mp=7, required_intelligence=20
-    )
-    skill_2 = Skill(
-        category_type='Attack', family_type='Frizz', new_name='Frizzle',
-        old_name='Blazemore', description='Inflict damage with giant fireball',
-        mp_cost=4, required_level=14, required_mp=46, required_intelligence=64,
-        upgrade=1  # added foreign key ID and value adds to table w/out problem
-    )
-    skill_3 = Skill(
-        category_type='Attack', family_type='Frizz', new_name='Kafrizzle',
-        old_name='Blazemost', description='Inflict damage with pillars of fire',
-        mp_cost=10, required_level=29, required_mp=112, required_intelligence=146,
-        upgrade=2  # added foreign key ID and value adds to table w/out problem
-    )
-    skill_4 = Skill(
-        category_type='Attack', family_type='Zap', new_name='Zap',
-        old_name='Bolt', description='Strikes all enemies with lightning',
-        mp_cost=5, required_level=7,required_mp=20, required_intelligence=35
-    )
-    skill_5 = Skill(
-        category_type='Attack', family_type='Zap', new_name='Lightning',
-        old_name='Lightning',
-        description='Calls on lightning attack to all enemies', mp_cost=3,
-        required_level=11, required_hp=65, required_attack=90,
-        required_speed=52
-    )
-    skill_6 = Skill(
-        category_type='Recovery', family_type='Heal', new_name='Heal',
-        old_name='Heal', description='Heals 30-40 Hp for one ally', mp_cost=2,
-        required_level=2, required_mp=7, required_intelligence=6
-    )
-
-    test_skills = [skill_1, skill_2, skill_3, skill_4, skill_5, skill_6]
-    
-    with Session(engine) as session:
-        for skill in test_skills:
-            session.add(skill)
-        # session.refresh(skill_4)
-        session.commit()
-
-    return test_skills
+    session.commit()
 
 
 def create_skill_combo(skills):
@@ -182,14 +106,14 @@ def create_breeds(families, monsters):
 
 def main():
     create_db_and_tables()
-    create_items()
-    families = create_monster_family()
-    skills = create_skill()
-    monsters = create_monster_detail(skills)
-    create_breeds(families, monsters)
-    create_skill_combo(skills)
-    create_monster_detail(skills)
+    create_item_csv()
+    create_monster_family_csv()
+    create_skill_csv()
+    # monsters = create_monster_detail(skills)
+    # create_breeds(families, monsters)
+    # create_skill_combo(skills)
+    # create_monster_detail(skills)
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
---
+++ pytest.py
@@ -0,0 +1,126 @@
+import unittest
+
+from database import create_db_and_tables, engine
+from sqlmodel import Session
+
+from model import Item, MonsterFamily, MonsterDetail, Skill, MonsterSkillLink,\
+    MonsterBreedingLink, SkillCombine
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+def create_items():
+    item_1 = Item(
+        item_name='Herb', item_category='Recovery',
+        item_description='Restores around 30 HP', price=10, sell_price=6,
+        sell_location='Bazaar shop 1'
+    )
+    item_2 = Item(
+        item_name='Lovewater', item_category='Recovery',
+        item_description='Restores around 70 HP', price=80, sell_price=60,
+        sell_location='Bazaar shop 1'
+    )
+    item_3 = Item(
+        item_name='Antidote', item_category='recovery',
+        item_description='Cures poison of one ally', price=10,
+        sell_price=8,
+        sell_location='Bazaar shop 1'
+    )
+    item_4 = Item(
+        item_name='Repellent', item_category='dungeon use',
+        item_description='Reduces monster encounters', price=200,
+        sell_price=150, sell_location='Bazaar shop 1'
+    )
+    item_5 = Item(
+        item_name='BeefJerky', item_category='meat',
+        item_description="Give to monster to tame during battle or reduce "
+                         "your own monster's WLD (wildness) by 5",
+        price=200, sell_price=150, sell_location='Bazaar shop 1'
+    )
+    test_items = [item_1, item_2, item_3, item_4, item_5]
+    session = Session(engine)
+
+    for item in test_items:
+        session.add(item)
+
+    session.commit()
+
+
+def create_monster_family():
+    family_1 = MonsterFamily(family_eng='SLIME')
+    family_2 = MonsterFamily(family_eng='DRAGON')
+    family_3 = MonsterFamily(family_eng='BEAST')
+    family_4 = MonsterFamily(family_eng='BIRD')
+    family_5 = MonsterFamily(family_eng='PLANT')
+    family_6 = MonsterFamily(family_eng='BUG')
+    family_7 = MonsterFamily(family_eng='DEVIL')
+    family_8 = MonsterFamily(family_eng='UNDEAD')
+    family_9 = MonsterFamily(family_eng='MATERIAL')
+    family_10 = MonsterFamily(family_eng='???')
+
+    test_families = [family_1, family_2, family_3, family_4, family_5,
+                     family_6, family_7, family_8, family_9, family_10]
+    with Session(engine) as session:
+        for family in test_families:
+            session.add(family)
+
+        session.commit()
+
+    return [family_1, family_2, family_3, family_4, family_5,
+            family_6, family_7, family_8, family_9, family_10]
+
+
+def create_skill():
+    skill_1 = Skill(
+        category_type='Attack', family_type='Frizz', new_name='Frizz',
+        old_name='Blaze', description='Inflict damage with small fireball ',
+        mp_cost=2, required_level=2, required_mp=7, required_intelligence=20
+    )
+    skill_2 = Skill(
+        category_type='Attack', family_type='Frizz', new_name='Frizzle',
+        old_name='Blazemore', description='Inflict damage with giant fireball',
+        mp_cost=4, required_level=14, required_mp=46, required_intelligence=64,
+        upgrade=1  # added foreign key ID and value adds to table w/out problem
+    )
+    skill_3 = Skill(
+        category_type='Attack', family_type='Frizz', new_name='Kafrizzle',
+        old_name='Blazemost',
+        description='Inflict damage with pillars of fire', mp_cost=10,
+        required_level=29, required_mp=112, required_intelligence=146,
+        upgrade=2
+        # added foreign key ID and value adds to table w/out problem
+    )
+    skill_4 = Skill(
+        category_type='Attack', family_type='Zap', new_name='Zap',
+        old_name='Bolt', description='Strikes all enemies with lightning',
+        mp_cost=5, required_level=7, required_mp=20, required_intelligence=35
+    )
+    skill_5 = Skill(
+        category_type='Attack', family_type='Zap', new_name='Lightning',
+        old_name='Lightning',
+        description='Calls on lightning attack to all enemies', mp_cost=3,
+        required_level=11, required_hp=65, required_attack=90,
+        required_speed=52
+    )
+    skill_6 = Skill(
+        category_type='Recovery', family_type='Heal', new_name='Heal',
+        old_name='Heal', description='Heals 30-40 Hp for one ally', mp_cost=2,
+        required_level=2, required_mp=7, required_intelligence=6
+    )
+
+    test_skills = [skill_1, skill_2, skill_3, skill_4, skill_5, skill_6]
+
+    with Session(engine) as session:
+        for skill in test_skills:
+            session.add(skill)
+        # session.refresh(skill_4)
+        session.commit()
+
+    return test_skills
+
+
+if __name__ == '__main__':
+    unittest.main()
