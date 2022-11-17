8a4fe4b880293e6768c205df84868ffe82f18baa
cmsato09 <chrismsato@gmail.com>
2022-11-16 23:40:35 -0800

    changed order of classes in model.py

--- model.py
+++ model.py
@@ -3,6 +3,9 @@ from sqlmodel import Field, Relationship, SQLModel
 
 
 class MonsterSkillLink(SQLModel, table=True):
+    """
+    many-to-many association table linking a monster to three different skills.
+    """
     id: Optional[int] = Field(default=None, primary_key=True)
     monster_id: Optional[int] = Field(
         default=None, foreign_key='monsterdetail.id',
@@ -12,75 +15,12 @@ class MonsterSkillLink(SQLModel, table=True):
     )
 
 
-class MonsterBreedingLinkBase(SQLModel):
-    """TODO: move stuff from MonsterBreedingLink here"""
-    child_id: Optional[int] = Field(
-        default=None, foreign_key='monsterdetail.id'
-    )
-    pedigree_id: Optional[int] = Field(
-        default=None, foreign_key='monsterdetail.id'
-    )
-    parent2_id: Optional[int] = Field(
-        default=None, foreign_key='monsterdetail.id'
-    )
-    pedigree_family_id: Optional[int] = Field(
-        default=None, foreign_key='monsterfamily.id'
-    )
-    family2_id: Optional[int] = Field(
-        default=None, foreign_key='monsterfamily.id'
-    )
-
-
-class MonsterBreedingLink(MonsterBreedingLinkBase, table=True):
-    """
-    This helped:
-    https://github.com/tiangolo/sqlmodel/issues/10#issuecomment-1002835506
-    """
-    id: Optional[int] = Field(default=None, primary_key=True)
-    child: "MonsterDetail" = Relationship(
-        sa_relationship_kwargs={
-            'primaryjoin': 'MonsterBreedingLink.child_id==MonsterDetail.id',
-            'lazy': 'joined'
-        }
-    )
-    pedigree: "MonsterDetail" = Relationship(
-        sa_relationship_kwargs={
-            'primaryjoin': 'MonsterBreedingLink.pedigree_id==MonsterDetail.id',
-            'lazy': 'joined'
-        }
-    )
-    parent2: "MonsterDetail" = Relationship(
-        sa_relationship_kwargs={
-            'primaryjoin': 'MonsterBreedingLink.parent2_id==MonsterDetail.id',
-            'lazy': 'joined'
-        }
-    )
-    pedigree_family: "MonsterFamily" = Relationship(
-        sa_relationship_kwargs={
-            'primaryjoin': 'MonsterBreedingLink.pedigree_family_id'
-                           '==MonsterFamily.id',
-            'lazy': 'joined'
-        }
-    )
-    family2: "MonsterFamily" = Relationship(
-        sa_relationship_kwargs={
-            'primaryjoin': 'MonsterBreedingLink.family2_id==MonsterFamily.id',
-            'lazy': 'joined'
-        }
-    )
-
-
-class MonsterBreedingLinkRead(MonsterBreedingLinkBase):
-    id: int
-
-
 class MonsterDetailBase(SQLModel):
     new_name: str
     old_name: str
     description: str
 
-    # one-to-many relation. monster in only one family category.
-    # many monsters in a family category
+    # one-to-many relation where a family is linked to many monsters
     family_id: int = Field(foreign_key='monsterfamily.id')
 
 
@@ -118,6 +58,68 @@ class MonsterFamilyReadWithMonsterDetail(MonsterFamilyRead):
     monsters: List[MonsterDetailRead] = []
 
 
+class MonsterBreedingLinkBase(SQLModel):
+    """TODO: move stuff from MonsterBreedingLink here"""
+    child_id: Optional[int] = Field(
+        default=None, foreign_key='monsterdetail.id'
+    )
+    pedigree_id: Optional[int] = Field(
+        default=None, foreign_key='monsterdetail.id'
+    )
+    parent2_id: Optional[int] = Field(
+        default=None, foreign_key='monsterdetail.id'
+    )
+    pedigree_family_id: Optional[int] = Field(
+        default=None, foreign_key='monsterfamily.id'
+    )
+    family2_id: Optional[int] = Field(
+        default=None, foreign_key='monsterfamily.id'
+    )
+
+
+class MonsterBreedingLink(MonsterBreedingLinkBase, table=True):
+    """
+    This helped:
+    https://github.com/tiangolo/sqlmodel/issues/10#issuecomment-1002835506
+    """
+    id: Optional[int] = Field(default=None, primary_key=True)
+    child: 'MonsterDetail' = Relationship(
+        sa_relationship_kwargs={
+            'primaryjoin': 'MonsterBreedingLink.child_id==MonsterDetail.id',
+            'lazy': 'joined'
+        }
+    )
+    pedigree: 'MonsterDetail' = Relationship(
+        sa_relationship_kwargs={
+            'primaryjoin': 'MonsterBreedingLink.pedigree_id==MonsterDetail.id',
+            'lazy': 'joined'
+        }
+    )
+    parent2: 'MonsterDetail' = Relationship(
+        sa_relationship_kwargs={
+            'primaryjoin': 'MonsterBreedingLink.parent2_id==MonsterDetail.id',
+            'lazy': 'joined'
+        }
+    )
+    pedigree_family: 'MonsterFamily' = Relationship(
+        sa_relationship_kwargs={
+            'primaryjoin': 'MonsterBreedingLink.pedigree_family_id'
+                           '==MonsterFamily.id',
+            'lazy': 'joined'
+        }
+    )
+    family2: 'MonsterFamily' = Relationship(
+        sa_relationship_kwargs={
+            'primaryjoin': 'MonsterBreedingLink.family2_id==MonsterFamily.id',
+            'lazy': 'joined'
+        }
+    )
+
+
+class MonsterBreedingLinkRead(MonsterBreedingLinkBase):
+    id: int
+
+
 class MonsterBreedingLinkReadWithParentsAndFamilies(MonsterBreedingLinkRead):
     pedigree: Optional[MonsterDetailRead]
     parent2: Optional[MonsterDetailRead]
@@ -145,14 +147,14 @@ class Skill(SkillBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
 
     upgrade_to_id: Optional[int] = Field(
-        foreign_key='skill.id',  # refers to database table name
+        foreign_key='skill.id',  # lowercase refers to database table name
         default=None,
     )
     upgrade_to: Optional['Skill'] = Relationship(
         sa_relationship_kwargs={
             'primaryjoin': 'Skill.upgrade_to_id==Skill.id',
             'lazy': 'joined',
-            'remote_side': 'Skill.id'  # refers to this Skill table class
+            'remote_side': 'Skill.id'  # uppercase refers to this Skill class
         }
     )
 
@@ -163,7 +165,7 @@ class Skill(SkillBase, table=True):
         sa_relationship_kwargs={
             'primaryjoin': 'Skill.upgrade_from_id==Skill.id',
             'lazy': 'joined',
-            'remote_side': 'Skill.id'  # refers to this Skill table class
+            'remote_side': 'Skill.id'
         }
     )
 
