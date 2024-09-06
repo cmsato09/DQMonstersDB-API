ce76cab7525d16c77bfe6f380c572d420d6c9b9b
cmsato09 <55464277+cmsato09@users.noreply.github.com>
2024-09-06 10:03:49 -0700

    create test_insert_monster_family

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ from sqlmodel import SQLModel, create_engine, Session
 from sqlmodel.pool import StaticPool
 
 from app.main import app, get_session
-from app.models import MonsterDetail, Item
+from app.models import MonsterDetail, MonsterFamily, Item
 
 
 def test_read_root():
@@ -43,10 +43,39 @@ def test_insert_monster(client: TestClient, session: Session):
     assert data_entry['description'] == monster_comparison['description']
     assert data_entry['family_id'] == monster_comparison['family_id']
     assert data_entry == monster_comparison
-    
+
+def test_insert_monster_family(client: TestClient, session: Session):
+    """
+    Test individual insertion of monster family data into MonsterFamilyBase Model
+    """
+    family_list = [
+        'SLIME',
+        'DRAGON',
+        'BEAST',
+        'BIRD',
+        'PLANT',
+        'BUG',
+        'DEVIL',
+        'UNDEAD',
+        'MATERIAL',
+        '???',
+    ]
+
+    for family in family_list:
+        session.add(MonsterFamily(family_eng=f'{family}'))
+    session.commit()
+
+    for i in range(1,11):
+        response = client.get(f'dqm1/family/{i}')
+        family_entry = response.json()
+
+        assert response.status_code == 200
+        assert family_entry['family_eng'] == family_list[i-1]
+
+
 def test_insert_item(client: TestClient, session: Session):
     """
-    Tests individual insertion of item data into database
+    Tests individual insertion of item data into Items table
     """
     session.add(Item(
         item_name='Herb',
