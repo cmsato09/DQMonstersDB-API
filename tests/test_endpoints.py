26310d7a4accf1b2e8680501070622cc896d2bd6
cmsato09 <55464277+cmsato09@users.noreply.github.com>
2024-09-08 22:21:40 -0700

    Update test_endpoints.py
    - create test for monsterandskill endpoint
    - create failure test for monsterandskill endpoint

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -52,8 +52,8 @@ def test_read_monster(client_module, load_all_csvdata):
         "family_id": 5,
         "id": 110,
         "family": {
-        "family_eng": "PLANT",
-        "id": 5
+            "family_eng": "PLANT",
+            "id": 5
         }
     }
 
@@ -68,6 +68,91 @@ def test_read_monster_fail(client_module, load_all_csvdata):
     """
     monsterID = 999
     response = client_module.get(f'dqm1/monsters/{monsterID}')
+
+    assert response.status_code == 404
+
+
+def test_read_monsterandskill(client_module, load_all_csvdata):
+    """ 
+    Tests retrieving monster detail and skill info from database by monster_id.
+    """
+    monsterID = 110
+    response = client_module.get(f'dqm1/monstersandskill/{monsterID}')
     monster_entry = response.json()
 
+    monster_comparison = {
+        "new_name": "Watabou",
+        "old_name": "Watabou",
+        "description": "A mischievous mystical creature",
+        "family_id": 5,
+        "id": 110,
+        "family": {
+            "family_eng": "PLANT",
+            "id": 5
+        },
+        "skills": [
+            {
+            "category_type": "Support",
+            "family_type": "Status support",
+            "new_name": "Hocus Pocus",
+            "old_name": "Chance",
+            "description": "Casts a random effect - Can be good or bad",
+            "mp_cost": 20,
+            "required_level": 40,
+            "required_hp": None,
+            "required_mp": 224,
+            "required_attack": None,
+            "required_defense": None,
+            "required_speed": None,
+            "required_intelligence": 236,
+            "id": 120
+            },
+            {
+            "category_type": "Support",
+            "family_type": "Map",
+            "new_name": "Whistle",
+            "old_name": "Whistle",
+            "description": "Summons monsters inside the dungeon to fight you",
+            "mp_cost": 0,
+            "required_level": 4,
+            "required_hp": None,
+            "required_mp": 28,
+            "required_attack": None,
+            "required_defense": None,
+            "required_speed": None,
+            "required_intelligence": 24,
+            "id": 152
+            },
+            {
+            "category_type": "Support",
+            "family_type": "Status support",
+            "new_name": "Follow Suit",
+            "old_name": "Imitate",
+            "description": "Copy and return every skill cast on Caster for 1 turn",
+            "mp_cost": 4,
+            "required_level": 21,
+            "required_hp": 147,
+            "required_mp": 147,
+            "required_attack": 126,
+            "required_defense": 126,
+            "required_speed": 126,
+            "required_intelligence": 126,
+            "id": 126
+            }
+        ]
+    }
+
+    assert response.status_code == 200
+    assert monster_entry == monster_comparison
+
+
+def test_read_monsterandskill_fail(client_module, load_all_csvdata):
+    """ 
+    Tests retrieving monster detail and skill info from database by monster_id.
+    monster_id greater than 215 or less than 1 is invalid.
+    """
+    monsterID = 999
+    response = client_module.get(f'dqm1/monstersandskill/{monsterID}')
+    monster_entry = response.json()
+    
     assert response.status_code == 404
