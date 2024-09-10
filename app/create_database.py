d30d366c77ac6f71cf2c38b14b803f49fbecd930
cmsato09 <55464277+cmsato09@users.noreply.github.com>
2024-09-10 13:00:55 -0700

    updated relative file paths to create database.db and access csv files

--- app/create_database.py
+++ app/create_database.py
@@ -1,9 +1,12 @@
 from database import create_db_and_tables, engine
 from sqlmodel import Session
+from pathlib import Path
 import csv
 from models import Item, MonsterFamily, MonsterDetail, Skill, MonsterSkillLink,\
     MonsterBreedingLink, SkillCombine
 
+current_dir = Path(__file__).resolve().parent
+csv_dir = current_dir.parent / 'csv_files'
 
 def _insert_data(csv_file, Model):
     """
@@ -19,34 +22,34 @@ def _insert_data(csv_file, Model):
 
 
 def create_item_csv():
-    _insert_data('csv_files/DQM1_items.csv', Item)
+    _insert_data(csv_dir / 'DQM1_items.csv', Item)
 
 
 def create_monster_family_csv():
-    _insert_data('csv_files/DQM1_monster_family.csv', MonsterFamily)
+    _insert_data(csv_dir / 'DQM1_monster_family.csv', MonsterFamily)
 
 
 def create_skill_csv():
-    _insert_data('csv_files/DQM1_skills.csv', Skill)
+    _insert_data(csv_dir / 'DQM1_skills.csv', Skill)
 
 
 def create_skillcombo_csv():
-    _insert_data('csv_files/DQM1_skill_combo.csv', SkillCombine)
+    _insert_data(csv_dir / 'DQM1_skill_combo.csv', SkillCombine)
 
 
 def create_monster_detail_csv():
-    _insert_data('csv_files/DQM1_monsterdetails.csv', MonsterDetail)
+    _insert_data(csv_dir / 'DQM1_monsterdetails.csv', MonsterDetail)
 
 
 def create_breed_combo():
-    _insert_data('csv_files/DQM1_breeding_combo.csv', MonsterBreedingLink)
+    _insert_data(csv_dir / 'DQM1_breeding_combo.csv', MonsterBreedingLink)
 
 
 def create_monster_skill_link():
-    _insert_data('csv_files/DQM1_monster_skill_link.csv', MonsterSkillLink)
+    _insert_data(csv_dir / 'DQM1_monster_skill_link.csv', MonsterSkillLink)
 
 
-def main():
+def load_all_csv_data():
     create_db_and_tables()
     create_item_csv()
     create_monster_family_csv()
@@ -58,4 +61,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    load_all_csv_data()
