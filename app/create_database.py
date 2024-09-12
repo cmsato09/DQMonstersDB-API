576a0763300a8325fb677a7502ff62c9959a33e8
cmsato09 <55464277+cmsato09@users.noreply.github.com>
2024-09-12 12:43:32 -0700

    Reformat create_database.py with ruff linter

--- app/create_database.py
+++ app/create_database.py
@@ -1,12 +1,22 @@
-from database import create_db_and_tables, engine
-from sqlmodel import Session
-from pathlib import Path
 import csv
-from models import Item, MonsterFamily, MonsterDetail, Skill, MonsterSkillLink,\
-    MonsterBreedingLink, SkillCombine
+from pathlib import Path
+
+from sqlmodel import Session
+
+from .database import create_db_and_tables, engine
+from .models import (
+    Item,
+    MonsterBreedingLink,
+    MonsterDetail,
+    MonsterFamily,
+    MonsterSkillLink,
+    Skill,
+    SkillCombine,
+)
 
 current_dir = Path(__file__).resolve().parent
-csv_dir = current_dir.parent / 'csv_files'
+csv_dir = current_dir.parent / "csv_files"
+
 
 def _insert_data(csv_file, Model):
     """
@@ -18,37 +28,37 @@ def _insert_data(csv_file, Model):
             reader = csv.DictReader(f)
             for row in reader:
                 # replace empty string with None
-                row = {k: (None if v == '' else v) for k, v in row.items()}
+                row = {k: (None if v == "" else v) for k, v in row.items()}
                 session.add(Model(**row))
         session.commit()
 
 
 def create_item_csv():
-    _insert_data(csv_dir / 'DQM1_items.csv', Item)
+    _insert_data(csv_dir / "DQM1_items.csv", Item)
 
 
 def create_monster_family_csv():
-    _insert_data(csv_dir / 'DQM1_monster_family.csv', MonsterFamily)
+    _insert_data(csv_dir / "DQM1_monster_family.csv", MonsterFamily)
 
 
 def create_skill_csv():
-    _insert_data(csv_dir / 'DQM1_skills.csv', Skill)
+    _insert_data(csv_dir / "DQM1_skills.csv", Skill)
 
 
 def create_skillcombo_csv():
-    _insert_data(csv_dir / 'DQM1_skill_combo.csv', SkillCombine)
+    _insert_data(csv_dir / "DQM1_skill_combo.csv", SkillCombine)
 
 
 def create_monster_detail_csv():
-    _insert_data(csv_dir / 'DQM1_monsterdetails.csv', MonsterDetail)
+    _insert_data(csv_dir / "DQM1_monsterdetails.csv", MonsterDetail)
 
 
 def create_breed_combo():
-    _insert_data(csv_dir / 'DQM1_breeding_combo.csv', MonsterBreedingLink)
+    _insert_data(csv_dir / "DQM1_breeding_combo.csv", MonsterBreedingLink)
 
 
 def create_monster_skill_link():
-    _insert_data(csv_dir / 'DQM1_monster_skill_link.csv', MonsterSkillLink)
+    _insert_data(csv_dir / "DQM1_monster_skill_link.csv", MonsterSkillLink)
 
 
 def load_all_csv_data():
