0cf18d48464409903ddc1b6f68ac976ff3161f74
Chris Sato <cmsato09.git@gmail.com>
2025-07-11 16:55:02 -0700

    Add Japanese transation table for Item

--- src/app/models/dqm1/item.py
+++ src/app/models/dqm1/item.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 
 
 class Item(SQLModel, table=True):
@@ -15,3 +15,22 @@ class Item(SQLModel, table=True):
     price: Optional[int] = Field(default=None)
     sell_price: Optional[int] = Field(default=None)
     sell_location: str
+
+    ja_translation: Optional["ItemJA"] = Relationship(backpopulates="item")
+
+
+class ItemJA(SQLModel, table=True):
+    """
+    Japanese translation for Item
+    """
+
+    __tablename__ = "item_ja"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name_ja: str
+    category_ja: str
+    description_ja: str
+    shop_ja: str
+
+    item_id: Optional[int] = Field(default=None, foreign_key="item.id")
+    item: Optional[Item] = Relationship(back_popluates="ja_translation")
